2a4f4e42086640af1540cccbedea91f4452a246f
daixj <chtomato@163.com>
2016-03-17 20:40:59 +0800

    test:测试用lua作为cgi，测试ok

--- htdocs/test.lua
+++ htdocs/test.lua
@@ -1,9 +1,12 @@
-#!/usr/bin/lua
+#!/usr/local/bin/lua
+--如果是在ubuntu上设置为#!/usr/bin/lua 
+print("Content-Type: text/html")
+print("")
 print ("<html>")
 print ("<head>")
-print ("<title>jjjjjjj</title>")
+print ("<title>test</title>")
 print ("</head>")
 print ("<body>")
-print ("nihao")
+print ("hello lua")
 print ("</body>")
 print ("</html>")
