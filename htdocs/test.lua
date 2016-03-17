6ba4e27dfe63ff027632dbedfdd42ab9e2fb185b
richard <chtomato@163.com>
2016-03-17 15:07:03 +0800

    httpd:add execute cgi function,need test this func

---
+++ htdocs/test.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/lua
+print ("<html>")
+print ("<head>")
+print ("<title>jjjjjjj</title>")
+print ("</head>")
+print ("<body>")
+print ("nihao")
+print ("</body>")
+print ("</html>")
--- httpd.c
+++ httpd.c
@@ -164,6 +164,117 @@ static serve_file(int client,const char *filename)
 	}
 	fclose(resource);
 }
+void bad_request(int client)  
+{  
+	char buf[1024];
+	/*回应客户端错误的 HTTP 请求 */  
+	sprintf(buf, "HTTP/1.0 400 BAD REQUEST\r\n");  
+	send(client, buf, sizeof(buf), 0);  
+	sprintf(buf, "Content-type: text/html\r\n");  
+	send(client, buf, sizeof(buf), 0);  
+	sprintf(buf, "\r\n");  
+	send(client, buf, sizeof(buf), 0);  
+	sprintf(buf, "<P>Your browser sent a bad request, ");  
+	send(client, buf, sizeof(buf), 0);  
+	sprintf(buf, "such as a POST without a Content-Length.\r\n");  
+	send(client, buf, sizeof(buf), 0);  
+}
+void cannot_execute(int client)  
+{  
+	char buf[1024]; 
+	/* 回应客户端 cgi 无法执行*/  
+	sprintf(buf, "HTTP/1.0 500 Internal Server Error\r\n");  
+	send(client, buf, strlen(buf), 0);  
+	sprintf(buf, "Content-type: text/html\r\n");  
+	send(client, buf, strlen(buf), 0);  
+	sprintf(buf, "\r\n");  
+	send(client, buf, strlen(buf), 0);  
+	sprintf(buf, "<P>Error prohibited CGI execution.\r\n");  
+	send(client, buf, strlen(buf), 0);  
+}
+static execute_cgi(int client, const char *path,const char *method,const char *query_string)
+{
+	int cgi_output[2];
+	int cgi_input[2];
+	char buf[1024];
+	pid_t pid;
+	int status;
+	int i;
+	char c;
+	int numchars = 1;
+	int content_length = -1;
+
+	buf[0] = 'A';
+	buf[1] = '\0';
+	if (strcasecmp(method,"GET") == 0){
+		while(numchars > 0 && strcmp("\n",buf))
+			numchars = get_line(client,buf,sizeof(buf));
+	}else{//POST
+		/*在POST的HTTP请求中找到Content-Length:*/
+		numchars = get_line(client,buf,sizeof(buf));
+		while(numchars > 0 && strcmp("\n",buf)){
+			buf[15] = '\0';
+			if(strcasecmp(buf,"Content-Length:") == 0)
+				content_length = atoi(&(buf[16]));
+			else
+				numchars = get_line(client,buf,sizeof(buf));
+		}
+		if(content_length == -1){
+			bad_request(client);
+			return;
+		}
+	}
+	/*ok情况下*/
+	sprintf(buf,"HTTP/1.0 200 OK\r\n");
+	send(client,buf,strlen(buf),0);
+
+	if(pipe(cgi_input) < 0){
+		cannot_execute(client);
+		return;
+	}
+	if(pipe(cgi_output) < 0){
+		cannot_execute(client);
+		return;
+	}
+	if((pid = fork()) < 0 ){
+		cannot_execute(client);
+		return;
+	}else if(pid == 0){
+		char meth_env[255];
+		char query_env[255];
+		char length_env[255];
+		dup2(cgi_output[1],1);
+		dup2(cgi_input[0],0);
+		close(cgi_output[0]);
+		close(cgi_input[1]);
+		/*设置request_method的环境变量*/
+		sprintf(meth_env,"REQUEST_METHOD=%s",method);
+		putenv(meth_env);
+		if(strcasecmp(method,"GET") == 0){
+			sprintf(query_env,"QUERY_STRING=%s",query_string);
+			putenv(query_env);
+		}else {
+			sprintf(length_env,"CONTENT-LENGTH=%d",content_length);
+			putenv(length_env);
+		}
+		execl(path,path,NULL);
+		exit(0);
+	}else {
+		close(cgi_output[1]);
+		close(cgi_input[0]);
+		if(strcasecmp(method,"POST") == 0)
+			for(i = 0;i < content_length; ++i){
+				recv(client,&c,1,0);
+				write(cgi_output[1],&c,1);
+			}
+		while(read(cgi_output[0],&c,1) > 0)
+			send(client,&c,1,0);
+		close(cgi_output[0]);
+		close(cgi_input[1]);
+		waitpid(pid,&status,0);
+	}
+
+}
 void accept_request(int client)
 {
 	char buf[1024];
@@ -229,10 +340,10 @@ void accept_request(int client)
 			strcat(path,"index.html");
 		if((st.st_mode & S_IXUSR /*文件所有则有可执行权限*/) || (st.st_mode & S_IXGRP /*用户组具可执行权限*/) || (st.st_mode & S_IXOTH/*其他用户具可执行权限*/))
 			cgi = 1;
-		//if(!cgi)
+		if(!cgi)
 			serve_file(client,path);
-		//else
-		//	execute_cgi(cl)
+		else
+			execute_cgi(client,path,method,query_string);
 	}
 	close(client);
 }
